4fd04be1f3d93e395e2df76fb9c97cb0a1d61ebc
Engel62 <sr.engel@bk.ru>
2022-10-17 14:05:19 +0300

    выполненая задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        //  Задача 1
+        System.out.println("Задача 1");
         double dog = 3;
         System.out.println(dog);
         dog = dog + 4;
